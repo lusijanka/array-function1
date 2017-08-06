d49ee10b9aea90b9c104d82ddd0c4a5ce61fac45
lusijanka <lucjamiodek@gmail.com>
2017-08-06 11:03:05 +0200

    new person added into array

---
+++ function.js
@@ -0,0 +1,27 @@
+// Kodilla way
+var female = ['Ana', 'Isabel', 'Caroline', 'Kate'];
+var male = ['Jacob', 'Peter', 'Mathew', 'Ben'];
+var mergedArrays = female.concat(male);
+console.log(mergedArrays);
+var newPerson = 'Ingrid';
+if (mergedArrays.indexOf(newPerson) === -1) {
+	mergedArrays.push(newPerson)
+	console.log('Name ' + newPerson + ' has been added and now the result is ' + mergedArrays);
+} else {
+	console.log('Name' + newPerson + 'already exists');
+};
+
+//my way
+var female = ['Ana', 'Isabel', 'Caroline', 'Kate'];
+var male = ['Jacob', 'Peter', 'Mathew', 'Ben'];
+var mergedArrays = female.concat(male);
+console.log(mergedArrays);
+var newPerson = 'Ingrid';
+for (i = 0; i < mergedArrays.length; i++) {
+	if (i == newPerson) {
+		break;
+	} else {
+		mergedArrays.push(newPerson);
+	};
+};		
+console.log(mergedArrays);
